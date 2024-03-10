9e68ceb5b77512962bf87e0d6ff854c2f36030b6
enghirushan <eng.hirushan@gmail.com>
2024-03-10 20:02:08 +0530

    initial commit

---
+++ src/app/page.jsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import img1 from "../../public/Profile-pic.jpg"
+export default function Home() {
+  return (
+    <>
+       {}
+       <Image
+
+
+       src ={img1}
+       />
+      
+       
+    </>
+  );
+}
